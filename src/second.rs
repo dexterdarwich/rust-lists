cb7cc68e86aa385610aab291a7c9166bd315ba66
Dexter Darwich <dex.darwich@gmail.com>
2020-08-26 21:39:19 -0600

    IterMut implementation

--- src/second.rs
+++ src/second.rs
@@ -16,6 +16,10 @@ pub struct Iter<'a, T> {
     next: Option<&'a Node<T>>,
 }
 
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>,
+}
+
 impl<T> List<T> {
     pub fn new() -> Self {
         List { head: None }
@@ -57,6 +61,10 @@ impl<T> List<T> {
         Iter { next: self.head.as_ref().map(|node| &**node) }
         // Or (deref coercion): Iter { next: self.head.as_ref().map::<&Node<T>, _>(|node| &node) }
     }
+
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+        IterMut { next: self.head.as_mut().map(|node| &mut **node)}
+    }
 }
 
 impl<T> Drop for List<T> {
@@ -77,11 +85,23 @@ impl<T> Iterator for IntoIter<T> {
 
 impl<'a, T> Iterator for Iter<'a, T> {
     type Item = &'a T;
+
     fn next(&mut self) -> Option<Self::Item> {
         self.next.map(|node| {
             self.next = node.next.as_ref().map(|node| &**node);
             // Or (deref coercion): self.next = node.next.as_ref().map::<&Node<T>, _>(|node| &node);
             &node.elem
+        })
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node|{
+            self.next = node.next.as_mut().map(|node| &mut **node);
+            &mut node.elem
         })
     }
 }
@@ -157,5 +177,16 @@ mod tests {
         assert_eq!(iter.next(), Some(&3));
         assert_eq!(iter.next(), Some(&2));
         assert_eq!(iter.next(), Some(&1));
+    }
+
+    #[test]
+    fn iter_mut() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter = list.iter_mut();
+        assert_eq!(iter.next(), Some(&mut 3));
+        assert_eq!(iter.next(), Some(&mut 2));
+        assert_eq!(iter.next(), Some(&mut 1));
     }
 }
